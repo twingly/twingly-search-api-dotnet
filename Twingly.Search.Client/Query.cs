4b20193a00372485cea91cf57c485272881eed82
Andrey Mironoff <mironoff@madlabs.lv>
2016-03-26 17:08:00 +0200

    Finish QueryBuilder

--- Twingly.Search.Client/Query.cs
+++ Twingly.Search.Client/Query.cs
@@ -11,13 +11,92 @@ namespace Twingly.Search.Client
     /// </summary>
     public class Query
     {
+        private string searchPattern = String.Empty;
+
         /// <summary>
         /// The pattern to use when searching for blog posts
         /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when the property is set to a null or empty string
+        /// </exception>
         public string SearchPattern
         {
+            get
+            {
+                return searchPattern;
+            }
+
+            internal set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentOutOfRangeException
+                        ("SearchPattern", "Please provide a non-empty string as a search pattern.");
+                this.searchPattern = value;
+            }
+        }
+
+        /// <summary>
+        /// Used to only include posts published after a certain UTC timestamp. 
+        /// </summary>
+        public DateTime? StartTime
+        {
             get;
-            internal set;
+            set;
+        }
+
+        /// <summary>
+        /// Used to exclude posts published after a certain UTC timestamp. 
+        /// </summary>
+        public DateTime? EndTime
+        {
+            get;
+            set;
+        }
+
+
+        public Language? Language
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Creates a new instance of a <see cref="Query"/>
+        /// with the given <paramref name="searchPattern"/>.
+        /// </summary>
+        /// <param name="searchPattern">The pattern to use when searching for blog posts</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when the <paramref name="searchPattern"/> is set to a null or empty string
+        /// </exception>
+        public Query(string searchPattern)
+        {
+            this.searchPattern = searchPattern;
+        }
+
+
+        internal void ThrowIfNotAValidStartTime(DateTime startTime)
+        {
+            // allow 'give me the freshest posts' queries by accounting for
+            // the delay between forming a query and the server actually receiving it.
+            if (startTime > DateTime.UtcNow.AddHours(1))
+                throw new ArgumentOutOfRangeException
+                    ("startTime", "Start time must be a UTC point in the past.");
+        }
+
+        internal void ThrowIfNotAValidEndTime(DateTime endTime)
+        {
+
+            if (this.StartTime.HasValue && endTime < this.StartTime)
+                throw new ArgumentOutOfRangeException
+                    ("endTime", "End time must be a UTC point in time that comes after the StartTime");
+        }
+
+        internal void ThrowIfInvalid()
+        {
+            if(this.StartTime.HasValue)
+                ThrowIfNotAValidStartTime(this.StartTime.Value);
+            if (this.EndTime.HasValue)
+                ThrowIfNotAValidEndTime(this.EndTime.Value);
         }
     }
 }
--- Twingly.Search.Client/QueryBuilder.cs
+++ Twingly.Search.Client/QueryBuilder.cs
@@ -13,9 +13,9 @@ namespace Twingly.Search.Client
     {
         private Query internalQuery = null;
 
-        private QueryBuilder()
+        private QueryBuilder(string searchPattern)
         {
-            internalQuery = new Query();
+            internalQuery = new Query(searchPattern);
         }
 
         /// <summary>
@@ -23,9 +23,12 @@ namespace Twingly.Search.Client
         /// </summary>
         /// <param name="searchPattern">The pattern to use when searching for blog posts</param>
         /// <returns>A new query builder</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when the specified search pattern is a null or empty string
+        /// </exception>
         public static QueryBuilder Create(string searchPattern)
         {
-            return new QueryBuilder().SearchPattern(searchPattern);
+            return new QueryBuilder(searchPattern);
         }
 
         /// <summary>
@@ -33,13 +36,74 @@ namespace Twingly.Search.Client
         /// </summary>
         /// <param name="searchPattern">The pattern to use when searching for blog posts</param>
         /// <returns>A half-baked query with the given <paramref name="searchPattern"/></returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when the specified search pattern is a null or empty string
+        /// </exception>
+        /// <see cref="https://developer.twingly.com/resources/search-language/"/>
         public QueryBuilder SearchPattern(string searchPattern)
         {
-            if (String.IsNullOrWhiteSpace(searchPattern))
-                throw new ArgumentNullException("searchPattern", "Please set this argument to a non-empty string");
             internalQuery.SearchPattern = searchPattern;
+            return this;
+        }
+
+        /// <summary>
+        /// Set to only include posts published after the given UTC timestamp
+        /// </summary>
+        /// <param name="targetDate">UTC time to be used as a starting point for search</param>
+        /// <returns>
+        /// A half-baked query with the given <paramref name="targetDate"/>
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when the <paramref name="targetDate"/> is a date in the future
+        /// </exception>
+        public QueryBuilder StartTime(DateTime targetDate)
+        {
+            internalQuery.ThrowIfNotAValidStartTime(targetDate);
+            internalQuery.StartTime = targetDate;
 
             return this;
         }
+
+        /// <summary>
+        /// Allows excluding posts older than the <paramref name="targetDate"/>
+        /// </summary>
+        /// <param name="targetDate">The UTC time that will be used to exclude older posts</param>
+        /// <returns>
+        /// A half-baked query with the given <paramref name="targetDate"/>
+        /// </returns>
+        public QueryBuilder EndTime(DateTime targetDate)
+        {
+            // we don't throw exceptions here, because an invalid end date
+            // may be due to the property setting order.
+            internalQuery.EndTime = targetDate;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Restricts the query to a specific language, for example 'en' or 'sv',
+        /// Note that some API keys only grant access to specific language(s)
+        /// </summary>
+        /// <param name="language">Document language</param>
+        /// <returns>
+        /// A half-baked query with the given <paramref name="language"/>
+        /// </returns>
+        public QueryBuilder Language(Language language)
+        {
+            internalQuery.Language = language;
+            return this;
+        }
+
+        /// <summary>
+        /// Returns the 
+        /// </summary>
+        /// <returns></returns>
+        public Query Build()
+        {
+            internalQuery.ThrowIfInvalid();
+
+            return internalQuery;
+        }
+
     }
 }
