fb9879ffd6a98a12979a3ae2b84aaa227c8542b7
Andrey Mironoff <mironoff@madlabs.lv>
2016-04-22 19:13:38 +0300

    Fix#10 remove unused method

--- Twingly.Search.Client/Infrastructure/Extensions.cs
+++ Twingly.Search.Client/Infrastructure/Extensions.cs
@@ -12,7 +12,7 @@ namespace Twingly.Search.Client.Infrastructure
 {
     internal static class Extensions
     {
-        private static ConcurrentDictionary<Type, XmlSerializer> serializerCache = 
+        private static readonly ConcurrentDictionary<Type, XmlSerializer> serializerCache = 
             new ConcurrentDictionary<Type, XmlSerializer>();
 
         public static T DeserializeXml<T>(this string xmlString)
@@ -52,37 +52,6 @@ namespace Twingly.Search.Client.Infrastructure
             }
 
             return returnValue;
-        }
-
-        public static object XmlDeserialize(Stream xml, Type objType)
-        {
-            StreamReader stream = null;
-            XmlTextReader reader = null;
-            XmlSerializer serializer;
-            if (serializerCache.ContainsKey(objType))
-            {
-                serializer = serializerCache[objType];
-            }
-            else
-            {
-                serializer = new XmlSerializer(objType);
-                serializerCache[objType] = serializer;
-            }
-
-            // avoid any side effects by keeping the stream position intact.
-            //long initialPosition = xml.Position;
-            //xml.Position = 0;
-            stream = new StreamReader(xml, Encoding.UTF8);
-            reader = new XmlTextReader(stream);
-            //xml.Position = initialPosition;
-
-            // leaving it to the caller code to dispose the stream.
-            // this shouldn't be an issue, because the readers
-            // will be eventually garbage-collected anyway,
-            // while the stream lifecycle management should be
-            // courtesy of the caller.
-            return serializer.Deserialize(reader);
-
         }
 
         public static string GetLanguageValue(this Language language)
