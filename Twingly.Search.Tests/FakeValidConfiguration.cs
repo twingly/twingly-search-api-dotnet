4daf96d2ef050b3aac42caa88be4c9c7fe92b814
Andrey Mironoff <mironoff@madlabs.lv>
2016-04-22 17:46:50 +0300

    Fix #1: add missing file.

--- Twingly.Search.Tests/FakeValidConfiguration.cs
+++ Twingly.Search.Tests/FakeValidConfiguration.cs
@@ -9,7 +9,9 @@ using Twingly.Search.Client.Domain;
 
 namespace Twingly.Search.Tests
 {
+    internal class FakeValidConfiguration : TwinglyConfiguration
     {
+        public FakeValidConfiguration() : base("som3r@ndomK3y", 700)
         {
             
         }
