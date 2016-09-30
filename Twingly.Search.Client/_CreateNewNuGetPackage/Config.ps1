0e68f6e31991f6c88bec5253518ac84928b6e6bd
Robin Wallin <robin@twingly.com>
2016-09-30 11:51:13 +0200

    Release v1.0.2
    Require well-formed user agent.

--- Twingly.Search.Client/_CreateNewNuGetPackage/Config.ps1
+++ Twingly.Search.Client/_CreateNewNuGetPackage/Config.ps1
@@ -3,7 +3,7 @@
 #
 # If you have modified this script:
 #	- if you uninstall the "Create New NuGet Package From Project After Each Build" package, this file may not be removed automatically; you may need to manually delete it.
-#	- if you update the "Create New NuGet Package From Project After Each Build" package, this file may not be updated unless you specify it to be overwritten, either by 
+#	- if you update the "Create New NuGet Package From Project After Each Build" package, this file may not be updated unless you specify it to be overwritten, either by
 #		confirming the overwrite if prompted, or by providing the "-FileConflictAction Overwrite" parameter when installing from the command line.
 #		If you overwrite this file then your custom changes will be lost, and you will need to manually reapply your changes.
 #		If you are not using source control, I recommend backing up this file before updating the package so you can see what changes you had made to it.
@@ -16,11 +16,11 @@
 # Specify the Version Number to use for the NuGet package. If not specified, the version number of the assembly being packed will be used.
 # NuGet version number guidance: https://docs.nuget.org/docs/reference/versioning and the Semantic Versioning spec: http://semver.org/
 # e.g. "" (use assembly's version), "1.2.3" (stable version), "1.2.3-alpha" (prerelease version).
-$versionNumber = "1.0.1"
+$versionNumber = "1.0.2"
 
-# Specify any Release Notes for this package. 
+# Specify any Release Notes for this package.
 # These will only be included in the package if you have a .nuspec file for the project in the same directory as the project file.
-$releaseNotes = "Target .NET 4.5.1 rather than 4.5.2 for mono compatibility"
+$releaseNotes = "Require well-formed user agent"
 
 # Specify a specific Configuration and/or Platform to only create a NuGet package when building the project with this Configuration and/or Platform.
 #	e.g. $configuration = "Release"
